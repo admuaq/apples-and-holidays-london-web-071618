b63ef52eef273c6d60d7ab1322b726349cc85a18
Adham Muhammad <admuaq@gmail.com>
2018-07-12 14:49:41 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,18 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  
+  holiday_hash.each do |key, value|
+    if key == :summer 
+      key.each do |hash, array|
+        puts array[last].join
+      end
+    end
+  end 
+        
+  
 end
+
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
